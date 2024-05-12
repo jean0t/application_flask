c3f698e9fdd9803356ed6e63bb6a9d13504338a6
jean0t <joaomouryf@hotmail.com>
2024-05-12 18:15:12 -0300

    fix: app, config and run

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, request
 
 from config import app_config, app_active
 
@@ -14,7 +14,7 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile("config.py")
 
-    app.config["SQLALCHEMY_DATBASE_URI"] = config.SQLALCHEMY_DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
     db = SQLAlchemy(config.APP)
     db.init_app(app)
@@ -22,5 +22,28 @@ def create_app(config_name):
     @app.route("/")
     def index():
         return "Hello World!"
+    
+    @app.route("/login/")
+    def login():
+        return "Tela de login"
+    
+    @app.route("/recovery-password/")
+    def recovery_password():
+        return "Tela de recuperar senha"
+    
+    @app.route("/profile", methods=["POST"])
+    def create_profile():
+        username = request.form["username"]
+        password = request.form["password"]
+
+        return "Perfil criado"
+    
+    @app.route("/profile/<int:id>", methods=["PUT"])
+    def edit_total_profile(id):
+        username = request.form["username"]
+        password = request.form["password"]
+
+        return f"Editado usuário cujo ID é {id}"
+    
 
     return app
--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import sys
 from app import create_app
 from config import app_config, app_active
 
@@ -7,4 +6,3 @@ config.APP = create_app(app_active)
 
 if __name__ == "__main__":
     config.APP.run(host=config.IP_HOST, port=config.PORT_HOST)
-    reload(sys)
